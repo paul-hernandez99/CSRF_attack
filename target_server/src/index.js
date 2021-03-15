5b5a6845d0fc45be260e2d3f3236a8e1cce8941b
fer <paul.hernandez@opendeusto.es>
2021-03-15 20:04:31 +0100

    img upload

--- target_server/src/index.js
+++ target_server/src/index.js
@@ -3,7 +3,6 @@ const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const session = require('express-session');
-const csfr = require('csfr');
 
 //initializations
 const app = express();
